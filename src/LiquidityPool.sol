b3cce9e372b250f665906555c3c4ac1f88cc97f7
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-05-20 11:19:36 -0300

    style: organizing using solidity style guide

--- src/LiquidityPool.sol
+++ src/LiquidityPool.sol
@@ -15,9 +15,8 @@ contract LiquidityPool is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
 
     ERC20 public s_token0;
     ERC20 public s_token1;
-
-    uint public s_reserve0;
-    uint public s_reserve1;
+    uint256 public s_reserve0;
+    uint256 public s_reserve1;
 
     constructor(
         address _token0,
@@ -25,28 +24,6 @@ contract LiquidityPool is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
     ) ERC20("Liquidity Pool Token", "LPT") Ownable(msg.sender) {
         s_token0 = ERC20(_token0);
         s_token1 = ERC20(_token1);
-    }
-
-    function _sqrt(uint256 y) private pure returns (uint256 z) {
-        if (y > 3) {
-            z = y;
-            uint x = y / 2 + 1;
-            while (x < z) {
-                z = x;
-                x = (y / x + x) / 2;
-            }
-        } else if (y != 0) {
-            z = 1;
-        }
-    }
-
-    function _min(uint256 x, uint256 y) private pure returns (uint256) {
-        return x < y ? x : y;
-    }
-
-    function _update(uint256 _reserve0, uint256 _reserve1) private {
-        s_reserve0 = _reserve0;
-        s_reserve1 = _reserve1;
     }
 
     function deposit(
@@ -86,7 +63,6 @@ contract LiquidityPool is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
     ) external nonReentrant returns (uint256 amount0, uint256 amount1) {
         uint256 bal0 = s_token0.balanceOf(address(this));
         uint256 bal1 = s_token1.balanceOf(address(this));
-
         uint256 totalSupply = totalSupply();
         amount0 = (_shares * bal0) / totalSupply;
         amount1 = (_shares * bal1) / totalSupply;
@@ -100,5 +76,27 @@ contract LiquidityPool is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
 
         s_token0.transfer(msg.sender, amount0);
         s_token1.transfer(msg.sender, amount1);
+    }
+
+    function _update(uint256 _reserve0, uint256 _reserve1) private {
+        s_reserve0 = _reserve0;
+        s_reserve1 = _reserve1;
+    }
+
+    function _sqrt(uint256 y) private pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
+    function _min(uint256 x, uint256 y) private pure returns (uint256) {
+        return x < y ? x : y;
     }
 }
