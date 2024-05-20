a83994c7be1fd7cb5f0f08f13ef782cfbf93e17f
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-05-20 14:26:46 -0300

    feature: create swap function

--- src/LiquidityPool.sol
+++ src/LiquidityPool.sol
@@ -9,14 +9,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract LiquidityPool is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
-    error Lp_NonConstantProduct();
+    error Lp_NonCPMMProduct();
     error Lp_ShareTooSmall();
     error Lp_ReceiveAmountToSmall(string message);
+    error Lp_InvalidTokenIn();
+    error Lp_InvalidTokenInAmount();
 
     ERC20 public s_token0;
     ERC20 public s_token1;
-    uint256 public s_reserve0;
+    uint256 public s_reserve0; //
     uint256 public s_reserve1;
+
+    uint256 public fee = 30; // represents 0.30%
 
     constructor(
         address _token0,
@@ -26,13 +30,14 @@ contract LiquidityPool is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
         s_token1 = ERC20(_token1);
     }
 
+    // @dev used by liquidity providers to deposit tokens into the pool
     function deposit(
         uint256 _amount0,
         uint256 _amount1
     ) external nonReentrant returns (uint256 shares) {
         if (s_reserve0 > 0 || s_reserve1 > 0) {
             if (_amount0 * s_reserve1 != _amount1 * s_reserve0) {
-                revert Lp_NonConstantProduct();
+                revert Lp_NonCPMMProduct();
             }
         }
 
@@ -58,6 +63,7 @@ contract LiquidityPool is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
         _update(s_reserve0 + _amount0, s_reserve1 + _amount1);
     }
 
+    // @dev used by liquidity providers to withdraw tokens from the pool
     function withdraw(
         uint256 _shares
     ) external nonReentrant returns (uint256 amount0, uint256 amount1) {
@@ -76,6 +82,46 @@ contract LiquidityPool is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
 
         s_token0.transfer(msg.sender, amount0);
         s_token1.transfer(msg.sender, amount1);
+    }
+
+    // @dev used by swapers to swap tokens
+    function swap(
+        address _tokenIn,
+        uint256 _amountIn
+    ) external returns (uint256 amountOut) {
+        bool isToken0 = _tokenIn == address(s_token0) ? true : false;
+        bool isToken1 = _tokenIn == address(s_token1) ? true : false;
+
+        if (!isToken0 && !isToken1) {
+            revert Lp_InvalidTokenIn();
+        }
+        if (_amountIn == 0) {
+            revert Lp_InvalidTokenInAmount();
+        }
+
+        (
+            IERC20 tokenIn,
+            IERC20 tokenOut,
+            uint256 reserveIn,
+            uint256 reserveOut
+        ) = isToken0
+                ? (s_token0, s_token1, s_reserve0, s_reserve1)
+                : (s_token1, s_token0, s_reserve1, s_reserve0);
+
+        tokenIn.transferFrom(msg.sender, address(this), _amountIn);
+        /*
+            @dev subtract the fee from the amount client sent. Ex: if sent 100, real swap value will be 97
+            upscale the percentage to be able to work with integers (that's why fee is 30 instead of 0.30)
+        */
+        uint256 amountWithFee = (_amountIn * (10000 - fee)) / 10000;
+        // @dev Constant Product Market Maker (CPMM) to maintain the weight of both tokens in balance
+        amountOut = (reserveOut * amountWithFee) / (reserveIn * amountWithFee);
+        tokenOut.transferFrom(address(this), msg.sender, amountOut);
+
+        _update(
+            s_token0.balanceOf(address(this)),
+            s_token1.balanceOf(address(this))
+        );
     }
 
     function _update(uint256 _reserve0, uint256 _reserve1) private {
